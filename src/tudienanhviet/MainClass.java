b8a3c4b7bdfdd907edbf21ba2157c3ffd00eccce
Thanh Bình Bùi <thanhsangbui23032k@gmail.com>
2022-07-08 10:04:25 +0700

    commit1
    Khởi tạo và đọc file

---
+++ src/tudienanhviet/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tudienanhviet;
+
+/**
+ *
+ * @author sang
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("App");
+        MainJFrame mainScreen=new MainJFrame();
+        mainScreen.setVisible(true);
+    }
+}
